166d591b04eb4271f06039c0605e029f20b34281
Anton Klinger <klianc09@gmail.com>
2017-01-20 21:40:07 +0100

    Folder Structure and Steuermann-scene.

---
+++ Assets/Scripts/SteeringWheel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class SteeringWheel:MonoBehaviour {
+
+	public float returnToNullForce = 1f; 
+	public float rotateSpeed = 5f; 
+	public float maxRotation = 170; 
+	public float currentRotation = 0; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (currentRotation > maxRotation) {
+			currentRotation= maxRotation; 
+		}else if (currentRotation <  -maxRotation) {
+			currentRotation=  -maxRotation; 
+		}
+		Quaternion originalRot= transform.rotation; 
+		transform.rotation= originalRot * Quaternion.AngleAxis(currentRotation, Vector3.up); 
+		 if (currentRotation > returnToNullForce) {
+			 currentRotation -= returnToNullForce; 
+		 }else if (currentRotation < returnToNullForce) {
+			currentRotation += returnToNullForce; 
+		 }else {
+			 currentRotation= 0; 
+		 }
+	}
+
+	public void rotateFor(float delta) {
+		currentRotation += delta * rotateSpeed; 
+	}
+}
---
+++ Assets/Scripts/Throttle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throttle : MonoBehaviour {
+
+	public float returnToNullForce = 0;
+	public float maxValue = 100;
+	public float minValue = -100;
+
+	public float changeForce = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
