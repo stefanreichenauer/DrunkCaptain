3b90bea46aae7c6b2ce191ab5add90a1e7ea8d55
Anton Klinger <klianc09@gmail.com>
2017-01-20 23:10:17 +0100

    Debug Buttons for MainMenu.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
 	public void startGame(){
+		testP1();
+	}
+
+	public void testP1(){
+		SceneManager.LoadScene("CaptainsView");
+	}
+	public void testP2(){
 		SceneManager.LoadScene("SteuermannView");
 	}
 
