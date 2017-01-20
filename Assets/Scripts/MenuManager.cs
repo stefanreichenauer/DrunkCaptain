eeed62fccd42214ff05bca0dcf46ca38a2f8ea60
Anton Klinger <klianc09@gmail.com>
2017-01-20 23:00:31 +0100

    Add Main Menu scene.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void startGame(){
+		SceneManager.LoadScene("SteuermannView");
+	}
+
+	public void quitGame(){
+		Application.Quit();
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+}
