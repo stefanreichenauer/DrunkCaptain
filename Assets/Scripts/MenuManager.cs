557560f113ea45ac5bc03ef60e3c4a83568c4e42
stefanreichenauer <stefan-reichenauer@gmx.at>
2017-01-22 12:07:59 +0100

    Finished Scene Networking

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class MenuManager : MonoBehaviour {
+public class MenuManager : MonoBehaviour
+{
 
     public bool isCaptain = true;
 
@@ -13,40 +15,60 @@ public class MenuManager : MonoBehaviour {
 
     public string connectionIP = "127.0.0.1";
     public int connectionPort = 7777;
-    
+
     public string ip = "127.0.0.1";
 
     public GameObject buttons;
+    public GameObject IPbuttons;
 
+    public Text ipInput;
 
     public void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-	public void startGame(){
-		testP1();
-	}
+    public void startGame()
+    {
+        testP1();
+    }
 
-	public void testP1(){
+    public void testP1()
+    {
         //Captain/Server
+        isCaptain = true;
         buttons.SetActive(false);
         SceneManager.LoadScene("NetworkView");
 
     }
-	public void testP2()
+    public void testP2()
+    {
+        buttons.SetActive(false);
+        IPbuttons.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void backBtn()
+    {
+        buttons.SetActive(true);
+        IPbuttons.SetActive(false);
+
+    }
+
+    public void submitBtn()
     {
         //Helmsman/Client
         isCaptain = false;
+        ip = ipInput.text;
         buttons.SetActive(false);
         SceneManager.LoadScene("NetworkView");
-        
-    }
 
-	public void quitGame(){
-		Application.Quit();
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
-		#endif
-	}
+    }
 }
--- Assets/Scripts/NetworkStarter.cs
+++ Assets/Scripts/NetworkStarter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class NetworkStarter : MonoBehaviour
 {
@@ -42,6 +43,4 @@ public class NetworkStarter : MonoBehaviour
     {
 
     }
-
-    
 }
